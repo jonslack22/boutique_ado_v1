8be63bd1afeee1053ab64d1f1b98afe9eb33599b
jonslack22 <jonathanslack89@ymail.com>
2023-07-27 13:58:14 +0000

    Added basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -26,14 +26,17 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51NY8qhJDzhjdX3mUimRUz1AS33huL8zTcxoTGAfY2a6JkGSxQt4sfco6oSBjet8xSzNVgDQt0buNp338ViXBDLxp00VPvMIdbA',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
